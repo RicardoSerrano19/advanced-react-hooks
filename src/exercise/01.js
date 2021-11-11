191e77bb3522ee7b1e2ad76fa70d39c2afeae427
Ricardo Serrano <ricardo.ss@live.com>
2021-11-11 13:08:07 -0600

    exercise1(extra): Accept object or function

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,15 +3,15 @@
 
 import * as React from 'react'
 
-function countReducer(state, step){
-  const { count } = step;
-  return {count: count};
+function countReducer(state, action){
+  return {...state,
+          ...(typeof action === 'function' ? action(state) : action)}
 }
 
 function Counter({initialCount = 0, step = 1}) {
   const [state, setState] = React.useReducer(countReducer, {count: initialCount})
   const { count } = state;
-  const increment = () => setState({count: count + step})
+  const increment = () => setState(currentState => ({count: currentState.count + step}))
 
   return <button onClick={increment}>{count}</button>
 }
